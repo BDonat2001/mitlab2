689d57719d51949ad8601426405c22bd9d5c5d41
BDonat2001 <baranyos.donat@gmail.com>
2024-03-25 18:39:39 +0100

    testing SensorAlarmState Implementation fix

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -20,37 +20,26 @@ public class TrainSensorTest {
         mockTU = mock(TrainUser.class);
         mockTC = mock(TrainController.class);
         sensor = new TrainSensorImpl(mockTC, mockTU);
+        sensor.overrideSpeedLimit(150);
     }
 
     @Test
     public void absoluteSpeedLimitMinTest(){
-        sensor.overrideSpeedLimit(150);
-        verify(mockTU,times(0)).setAlarmState(true);
-        
         sensor.overrideSpeedLimit(-1);
         verify(mockTU,times(1)).setAlarmState(true);
     }
     @Test
     public void absoluteSpeedLimitMaxTest(){
-        sensor.overrideSpeedLimit(150);
-        verify(mockTU,times(0)).setAlarmState(true);
-        
         sensor.overrideSpeedLimit(555);
         verify(mockTU,times(1)).setAlarmState(true);
     }
     @Test
     public void relativeSpeedLimitLowerTest(){
-        sensor.overrideSpeedLimit(150);
-        verify(mockTU,times(0)).setAlarmState(true);
-        
         sensor.overrideSpeedLimit(50);
         verify(mockTU,times(1)).setAlarmState(true);
     }
     @Test
     public void relativeSpeedLimitGreaterTest(){
-        sensor.overrideSpeedLimit(150);
-        verify(mockTU,times(0)).setAlarmState(true);
-        
         sensor.overrideSpeedLimit(155);
         verify(mockTU,times(0)).setAlarmState(true);
     }
